cb14ebd8dfad4ca7901d57bdd8e15201b24ca5b9
Christian Nickerson <christian.nickerson@faculty.ai>
2024-09-14 18:32:40 +0100

    default vu-server key added

--- src/vu1_monitor/config/settings.py
+++ src/vu1_monitor/config/settings.py
@@ -1,6 +1,6 @@
 import os
 
-from dynaconf import Dynaconf  # type: ignore
+from dynaconf import Dynaconf, Validator  # type: ignore
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,4 +10,5 @@ settings = Dynaconf(
     root_path=current_directory,
     environments=True,
     env_switcher="VU1_ENV",
+    validators=[Validator("KEY", default="cTpAWYuRpA2zx75Yh961Cg")],  # default key provided by VU-server
 )
