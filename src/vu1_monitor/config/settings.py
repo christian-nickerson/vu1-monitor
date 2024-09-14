15b0e764464792f1d4d376139bb8e416a8c5f7cf
Christian Nickerson <christian.nickerson@faculty.ai>
2024-09-14 22:51:38 +0100

    defaults added as validators

--- src/vu1_monitor/config/settings.py
+++ src/vu1_monitor/config/settings.py
@@ -1,6 +1,6 @@
 import os
 
-from dynaconf import Dynaconf  # type: ignore
+from dynaconf import Dynaconf, Validator  # type: ignore
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,4 +10,16 @@ settings = Dynaconf(
     root_path=current_directory,
     environments=True,
     env_switcher="VU1_ENV",
+    validators=[
+        # server
+        Validator("server.hostname", default="localhost"),
+        Validator("server.port", default=5430),
+        Validator("server.logging_level", default="INFO"),
+        Validator("server.key", default="cTpAWYuRpA2zx75Yh961Cg"),
+        # dials
+        Validator("cpu.name", default="CPU"),
+        Validator("gpu.name", default="GPU"),
+        Validator("memory.name", default="MEMORY"),
+        Validator("network.name", default="NETWORK"),
+    ],
 )
